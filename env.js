131f6438147d774c22f7b7552a162108b3f59e0b
Chentai-Tech <engineering@chentai-tech.com.tw>
2024-03-08 09:24:44 +0800

    add output message

--- env.js
+++ env.js
@@ -1,9 +1,11 @@
 var script_url = "https://script.google.com/macros/s/AKfycbztxJjRAliKQvtIN5sRjvWyvO5MQyY3yKmbVZhHnicp-hQBECw03DZM4qjCSmPsw7Zc-Q/exec";
 $(document).ready(function ($) {
     $("#google-sheet").submit(function (event) {
-        //$('#date, #time, #name, #phone').prop("disabled", false);
 		$('#name').prop("disabled", false);
         var name = $("#name").val();
+		const user = $("#name").val();
+		google.script.run.withSuccessHandler(displayMessage).processUserInput(user);
+		
 		var url = script_url + "?callback=ctrlq&name=" + name + "&action=create";
         var request = $.ajax({
             url: url,
@@ -15,7 +17,11 @@ $(document).ready(function ($) {
         request.always(function () {
 			$('#name').prop("disabled", true);
         });
-		//alert("登錄完成!!");
-        event.preventDefault();
+        event.preventDefault();	// 防止預設的表單提交行為
     })
 })
+
+function displayMessage(message) {
+	const outputDiv = $("#message");
+	outputDiv.textContent = message;
+}
